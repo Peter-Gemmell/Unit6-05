6733d252582c1e917df789751010249f4a45277a
Peter Gemmell <peter.gemmell@ocsbstudent.ca>
2021-01-06 21:15:04 +0000

    vv

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-  // defining variable of day and the prompt input for it
+// defining variable of day and the prompt input for it
 const day = prompt('What day is it Today? (all lower case)')
-  // defining variable of age and the prompt input for it
+// defining variable of age and the prompt input for it
 const age = prompt('How old are you?')
-  // if day isnt saturday and isnt sunday and under 18
+// if day isnt saturday and isnt sunday and under 18
 if ((day !== 'saturday' && day !== 'sunday') && (age < 18)) {
   // then display 'you have school today'
   document.write('You have school today')
@@ -11,7 +11,7 @@ if ((day !== 'saturday' && day !== 'sunday') && (age < 18)) {
   // then display 'you have work today'
   document.write('You have work today!')
   // else if its saturday or sunday
-}else if (day === 'saturday' || day === 'sunday') {
+} else if (day === 'saturday' || day === 'sunday') {
   // display 'its the weekend so relax'
   document.write('Relax for the weekend!')
 }
